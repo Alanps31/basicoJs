31f79b7a2c49e5e2b517dd48d6857037c7abbba4
Alan P. Santos <alan.pinheirodossantos@gmail.com>
2023-09-28 04:29:56 +0000

    gitpod teste

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-class People {
-    constructor(nome) {
-        this.nome = nome;
-    }
-}
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello Gitpod');
+});
+
+app.listen(3030, (err) => {
+  if (err) {
+    console.log('erro no app');
+  } else {
+    console.log('servidor rodando na porta 3030');
+  }
+});
